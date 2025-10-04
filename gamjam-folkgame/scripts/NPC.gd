c56491d0ca3583e5df18cd7c8648b79e0ce7cc54
mischievousVoid <summer.vantas69@gmail.com>
2025-10-04 22:54:17 +0300

    movement correct

--- gamjam-folkgame/scripts/NPC.gd
+++ gamjam-folkgame/scripts/NPC.gd
@@ -1,18 +1,23 @@
 extends Node3D
 
-@export var kuusk: bool
-@export var manyKuusk: bool
+@export var interactable: bool
+
+@onready var Cast: ShapeCast3D = $CharacterBody3D/ShapeCast3D
+@onready var nameNPC: Label3D = $Name
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$kuusk.hide()
-	
-	if kuusk:
-		$kuusk.show()
+	nameNPC.hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	
+	playerHere()
 	pass
+
+func playerHere():
+	if Cast.is_colliding():
+		nameNPC.show()
+	else:
+		nameNPC.hide()
