16a7264ee49ed109e9e3acbdff23e2f1b23f0b88
mischievousVoid <summer.vantas69@gmail.com>
2025-10-04 18:15:44 +0300

    trees

---
+++ gamjam-folkgame/scripts/dialogic_control.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func dialogStart():
+	#get_tree().paused = true
+	globalVariables.canPlayerMove = false
+	Dialogic.start('test')
+	get_viewport().set_input_as_handled()
+	Dialogic.timeline_ended.connect(end)
+
+func end():
+	Dialogic.timeline_ended.disconnect(end)
+	globalVariables.canPlayerMove = true
+	#get_tree().paused = false
