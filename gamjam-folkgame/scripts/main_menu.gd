0b281e620e6493af0633d78d03480eff71d40342
mischievousVoid <summer.vantas69@gmail.com>
2025-10-05 15:57:56 +0300

    main menu

--- gamjam-folkgame/scripts/main_menu.gd
+++ gamjam-folkgame/scripts/main_menu.gd
@@ -14,7 +14,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_play_pressed() -> void:
-	#get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://scenes/scene_1_forest.tscn")
 	pass
 
 func _on_options_pressed() -> void:
