da0c5d43756abf9515218a1b1cd094969245cd31
mischievousVoid <summer.vantas69@gmail.com>
2025-10-05 15:26:56 +0300

    animation

--- gamjam-folkgame/scripts/player_protagonist_body.gd
+++ gamjam-folkgame/scripts/player_protagonist_body.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@onready var player_protagonist_animated: AnimatedSprite3D = $PlayerProtagonistAnimated
 
 
 func _physics_process(delta):
@@ -17,6 +18,18 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction.x > 0:
+		player_protagonist_animated.flip_h = true
+	elif direction.x < 0:
+		player_protagonist_animated.flip_h = false
+	
+	if not direction:
+		player_protagonist_animated.play("idle")
+	else:
+		player_protagonist_animated.play("walk")
+		
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
