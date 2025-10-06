595eb4f30215d0bbf732e09a0b54cfc3362476f6
mischievousVoid <summer.vantas69@gmail.com>
2025-10-06 18:38:53 +0300

    honhonhon strawberry
    added strawberry placeholder and dialogue

--- gamjam-folkgame/scripts/metsaMaasika.gd
+++ gamjam-folkgame/scripts/metsaMaasika.gd
@@ -1,41 +1,39 @@
 extends Node3D
 
+@onready var strawberry: Label3D = $Label3D
+@onready var Cast: ShapeCast3D = $ShapeCast3D
+
+var interactionPossible = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-
+	strawberry.hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	playerHere()
 	pass
 
 func _input(event):
 	if event.is_action_pressed("interact"):
-		
-		if $Fox.interactionPossible:
-			if globalVariables.dialogueProgFox < 1:
-				foxDialogOne()
-			print("input")
-		elif $Eva.interactionPossible:
-			if globalVariables.dialogueProgEva < 1:
-				evaDialogOne()
-			print("input")
-		pass
+		if interactionPossible:
+			if globalVariables.dialogueProg == 3:
+				strawberryQuest()
+			print("strawberry")
+			pass
+			
+func playerHere():
+	if Cast.is_colliding():
+		strawberry.show()
+		interactionPossible = true
+	else:
+		strawberry.hide()
+		interactionPossible = false
 
-func foxDialogOne():
+func strawberryQuest():
 	globalVariables.canPlayerMove = false
-	Dialogic.start("A1S1D1")
-	globalVariables.dialogueProgFox += 1
-	globalVariables.dialogueProg += 1
-	get_viewport().set_input_as_handled()
-	Dialogic.timeline_ended.connect(end)
-
-func evaDialogOne():
-	globalVariables.canPlayerMove = false
-	Dialogic.start("A1S1D2")
-	globalVariables.dialogueProgEva += 1
+	Dialogic.start("A1S3D1")
 	globalVariables.dialogueProg += 1
 	get_viewport().set_input_as_handled()
 	Dialogic.timeline_ended.connect(end)
